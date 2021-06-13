a9c44792a92cc0ba5936b4ecba407d0d20e27806
Nuthan <inbox.nuthan@gmail.com>
2021-06-13 21:53:13 +0530

    Configure XML Comments in Startup.cs

--- swaggerDemo/Startup.cs
+++ swaggerDemo/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,7 +31,24 @@ namespace swaggerDemo
             services.AddControllers();
 
             services.AddSwaggerGen(swagger =>{
-                swagger.SwaggerDoc("v1", new OpenApiInfo{Title="Weather Forecast", Version="v1"});
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Weather Forecast API",
+                    Description = "Returns weather forecast information",
+                    TermsOfService = new Uri("https://go.microsoft.com/fwlink/?LinkID=206977"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nuthan Murarysetty",
+                        Email = string.Empty,
+                        Url = new Uri("https://techinuthan.blogspot.com")
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                swagger.IncludeXmlComments(xmlPath);
             });
         }
 
